3c43bae0677d13dc98554822b6b19461edbfcc68
Hoa <nguyenhoa17109@gmail.com>
2021-04-20 00:00:13 +0700

    Initial commit

---
+++ app/src/main/java/com/nguyenhoa/weatherforcast/fragment/FragmentBottomAdapter.java
@@ -0,0 +1,34 @@
+package com.nguyenhoa.weatherforcast.fragment;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.nguyenhoa.weatherforcast.fragment_home.FragmentHome;
+
+public class FragmentBottomAdapter extends FragmentStatePagerAdapter {
+    private int numPage = 4;
+    public FragmentBottomAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position){
+            case 0: return new FragmentMainHome();
+            case 1: return new FragmentCare();
+            case 2: return new FragmentNotification();
+            case 3: return new FragmentUser();
+            default: return new FragmentMainHome();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return numPage;
+    }
+
+
+}
